3dbf1c9e718eaa08db2e0ce192eed221ee60c52a
leer89 <leer89@gmail.com>
2016-10-31 19:10:20 -0400

    Add project files.

---
+++ SourceControlTest/SourceControlTest.cpp
@@ -0,0 +1,14 @@
+// SourceControlTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	using namespace std;
+
+	cout << "Randy Lee, Source Control Test";
+    return 0;
+}
+

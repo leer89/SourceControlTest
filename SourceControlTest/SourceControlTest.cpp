ddedb6a822315a6bf7c877e52f1b80e767e57bce
leer89 <leer89@gmail.com>
2016-10-31 19:52:30 -0400

    string addition example for git hub

--- SourceControlTest/SourceControlTest.cpp
+++ SourceControlTest/SourceControlTest.cpp
@@ -2,13 +2,32 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
 
+using namespace std;
+
+string returnName(string x1, string x2);
 
 int main()
 {
-	using namespace std;
+	string x1, x2, newName;
 
-	cout << "Randy Lee, Source Control Test";
-    return 0;
+	cout << "Randy Lee, Source Control Test" << endl;
+	cout << "Type in two words: " << endl;
+
+	cin >> x1;
+	cin >> x2;
+
+	newName = returnName(x1, x2);
+
+	cout << "My new name is: " << newName << endl;
+
+	return 0;
 }
 
+string returnName(string x1, string x2)
+{
+	return x1 + " " + x2;
+}
